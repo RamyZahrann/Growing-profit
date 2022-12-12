8d72abe2ef515a19ab60f777c3edb77c5211e0d3
RamyZahrann <ramyzahran2006@gmail.com>
2022-12-12 22:33:10 +0200

    Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,9 +13,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class nbb extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  final Screens = [
+    Center(child: Text("abc")),
+    Center(child: Text("Favourite")),
+    Center(child: Text("HOME")),
+    Center(child: Text("HOME")),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello"));
+    return Scaffold(
+        body: Screens[index],
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: GNav(
+                selectedIndex: index,
+                onTabChange: (index) => setState(() => this.index = index),
+                gap: 8,
+                backgroundColor: Colors.black,
+                color: Colors.white,
+                activeColor: Colors.white,
+                tabBackgroundColor: Colors.grey.shade800,
+                padding: EdgeInsets.all(16),
+                tabs: const [
+                  GButton(
+                    icon: Icons.home,
+                    text: "Home",
+                  ),
+                  GButton(
+                    icon: Icons.favorite_border_outlined,
+                    text: "Favourite",
+                  ),
+                  GButton(icon: Icons.search, text: "Search"),
+                  GButton(
+                    icon: Icons.settings,
+                    text: "Settings",
+                  ),
+                ]),
+          ),
+        ));
   }
 }
 
@@ -31,7 +77,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Future.delayed(const Duration(seconds: 5)).then((value) => {
           Navigator.of(context).pushReplacement(
-              CupertinoPageRoute(builder: ((context) => nbb())))
+              CupertinoPageRoute(builder: ((context) => HomePage())))
         });
   }
 
@@ -44,13 +90,13 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
             Image(
-              image: AssetImage("Images/img.png"),
-              width: 600,
+              image: AssetImage("Images/logo-removebg.png"),
+              width: 800,
             ),
             SizedBox(
               height: 50,
             ),
-            SpinKitThreeBounce(
+            SpinKitFadingFour(
               color: Color.fromARGB(255, 18, 78, 7),
               size: 50.0,
             )
